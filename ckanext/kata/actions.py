23750ee93a6bc7550ae6b8de30616e55861d7768
Aleksi Suomalainen <aleksi.suomalainen@nomovok.com>
2013-01-07 11:53:11 +0200

    Don't check for the related items if erelated is empty.

--- ckanext/kata/actions.py
+++ ckanext/kata/actions.py
@@ -9,10 +9,11 @@ def package_show(context, data_dict):
     pkg = Package.get(pkg_dict['id'])
     if 'erelated' in pkg.extras:
         erelated = pkg.extras['erelated']
-        for value in erelated.split(';'):
-            if len(Session.query(Related).filter(Related.title == value).all()) == 0:
-                data_dict = {'title': value,
-                             'type': _("Paper"),
-                             'dataset_id': pkg_dict['id']}
-                related_create(context, data_dict)
+        if len(erelated):
+            for value in erelated.split(';'):
+                if len(Session.query(Related).filter(Related.title == value).all()) == 0:
+                    data_dict = {'title': value,
+                                 'type': _("Paper"),
+                                 'dataset_id': pkg_dict['id']}
+                    related_create(context, data_dict)
     return pkg_dict
