5305876f067f3bc88e4fe27747848d240a83131a
Aleksi Suomalainen <aleksi.suomalainen@nomovok.com>
2013-03-05 15:58:46 +0200

    Contact form, part 1.

--- ckanext/kata/utils.py
+++ ckanext/kata/utils.py
@@ -66,3 +66,21 @@ def convert_to_text(resource, resource_fname):
         p.communicate()
         return convert_fd, convert_path
     return None, None
+
+
+def send_contact_email(owner, requestee, pkg, message):
+    msg = _("""%s (%s) is requesting access to study material for dataset you have created
+    %s.
+
+The message is as follows:
+%s
+""")
+    body = msg % (requestee.name,\
+                  requestee.email,\
+                  pkg.title if pkg.title else pkg.name,\
+                  message)
+    email_dict = {}
+    email_dict["subject"] = _("Material access request for dataset %s" % pkg.title\
+                              if pkg.title else pkg.name)
+    email_dict["body"] = body
+    send_notification(owner.as_dict(), email_dict)
