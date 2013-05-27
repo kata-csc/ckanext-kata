2e6c61b53ca5c7853fd75ea1d663d93354c0e1dc
Mikko Koho <mikko.koho@csc.fi>
2013-05-27 17:20:18 +0300

    Some comments for dummy test.

--- ckanext/kata/tests/test_kata.py
+++ ckanext/kata/tests/test_kata.py
@@ -1,5 +1,12 @@
+"""Test classes for KATA (CKAN Extension)."""
+
 from unittest import TestCase
 
 class TestKataExtension(TestCase):
+    """Tests for KATA (CKAN Extension).
+    
+    """
+    
     def test_reality_check(self):
+        """Dummy test which should never fail."""
         self.assertEqual(1+1, 2)
