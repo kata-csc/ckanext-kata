9dad80153c568b7079163a54b7b966c5c3eb01d0
Mikko Koho <mikko.koho@csc.fi>
2013-06-04 12:31:45 +0300

    Some docstrings to ckanext-kata

--- ckanext/kata/__init__.py
+++ ckanext/kata/__init__.py
@@ -1,4 +1,13 @@
-# this is a namespace package
+"""
+Kata extension for CKAN. 
+
+Provides our modified Add Dataset (package) page and contains many other modifications described here. This extension 
+contains also jQuery files, custom css (eg. kata.css) and several templates are overwritten from basic CKAN to provide 
+the Kata/TTA looks. Note, that some css and template modifications were done in early stage in CSC. Note that during 
+the software development there has been a great deal of regression problems in the extension and it might be 
+worthwhile remembering this when debugging some random odd bug. 
+"""
+
 try:
     import pkg_resources
     pkg_resources.declare_namespace(__name__)
--- ckanext/kata/tests/test_kata.py
+++ ckanext/kata/tests/test_kata.py
@@ -1,12 +1,18 @@
-"""Test classes for KATA (CKAN Extension)."""
+"""Test classes for Kata CKAN Extension."""
 
 from unittest import TestCase
+from ckanext.kata import actions 
 
 class TestKataExtension(TestCase):
-    """Tests for KATA (CKAN Extension).
-    
-    """
+    """General tests for Kata CKAN Extension."""
     
     def test_reality_check(self):
         """Dummy test which should never fail."""
         self.assertEqual(1+1, 2)
+        
+
+class TestKataActions(TestCase):
+    """Tests for Kata CKAN Extension's actions.py."""
+    
+    pass
+
