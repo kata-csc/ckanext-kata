02709029e700090b3300958f0a26d640aceaf6d6
Mikko Koho <mikko.koho@csc.fi>
2013-07-18 16:11:04 +0300

    FIELD_TITLES to setting.py

--- ckanext/kata/settings.py
+++ ckanext/kata/settings.py
@@ -1,8 +1,18 @@
 '''Settings and constants for Kata CKAN extension'''
 
-FACETS = ['groups','tags','extras_fformat','license','authorstring','organizationstring','extras_language']
+# Facets used in Solr queries
 # Facets need also to be changed to search.html. This all should be fixed in newer CKAN versions with IFacets interface.
+FACETS = ['groups','tags','extras_fformat','license','authorstring','organizationstring','extras_language']
 
+# Default sorting method. Pre-selects the corresponding option on search form.
 DEFAULT_SORT_BY = u'metadata_modified desc'
 
-
+FIELD_TITLES = {'organizationstring': _('Organization'),
+                'tags': _('Keywords'),
+                'extras_fformat': _('File formats'),
+                'groups': _('Discipline'),
+                'license': _('Licence'),
+                'authorstring': _('Author'),
+                'extras_language': _('Language'),
+                'title': _('Title'),
+                }
