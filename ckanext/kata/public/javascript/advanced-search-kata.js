9128107dc8f0162c1a7282c93627bb304be616bc
Mikko Koho <mikko.koho@csc.fi>
2013-07-23 11:51:33 +0300

    Advanced search interface beta

--- ckanext/kata/public/javascript/advanced-search-kata.js
+++ ckanext/kata/public/javascript/advanced-search-kata.js
@@ -4,14 +4,17 @@
 this.ckan.module('advanced-search-kata', function (jQuery, _) {
   return {
     options: {
-      target: 'select'
+      target: 'select.kata-search-by'
     },
 
     initialize: function () {
       var _this = this;
 
       this.el.on('change', this.options.target, function () {
-        jQuery( "#advanced-search-1-text" ).attr('name', this.value);
+        temp_arr = this.id.split('-');
+        index = temp_arr[temp_arr.length - 1];
+        console.log(index);
+        jQuery( "#advanced-search-text-" + index ).attr('name', this.value);
       });
     }
   };
@@ -26,11 +29,11 @@ this.ckan.module('search-toggle', function (jQuery, _) {
       this.el.on('click', function () {
 
         if (this.id == 'toggle_advanced') {
-          jQuery('.advanced_search_toggled').show();
-          jQuery('.basic_search_toggled').hide();
+          jQuery('#content .advanced_search_toggled').show();
+          jQuery('#content .basic_search_toggled').hide();
         } else {
-          jQuery('.advanced_search_toggled').hide();
-          jQuery('.basic_search_toggled').show();
+          jQuery('#content .advanced_search_toggled').hide();
+          jQuery('#content .basic_search_toggled').show();
         }
 
       });
@@ -40,11 +43,28 @@ this.ckan.module('search-toggle', function (jQuery, _) {
 
 toggle_search = function(type) {
   if (type == 'advanced') {
-    $('.advanced_search_toggled').show();
-    $('.basic_search_toggled').hide();
+    $('#content .advanced_search_toggled').show();
+    $('#content .basic_search_toggled').hide();
   } else {
-    $('.advanced_search_toggled').hide();
-    $('.basic_search_toggled').show();
+    $('#content .advanced_search_toggled').hide();
+    $('#content .basic_search_toggled').show();
+  }
+}
+
+add_search_elements = function(index) {
+  if (!$("#advanced-search-row-" + (index + 1)).length) {
+    cloned_row = $("#advanced-search-row-" + index).clone();
+
+    //console.log($("#advanced-search-row-" + index));
+    //cloned_row.html(cloned_row.html().replace('advanced-search-row-' + index, 'advanced-search-row-' + (index + 1)));
+    cloned_row.html(cloned_row.html().replace('advanced-search-text-' + index, 'advanced-search-text-' + (index + 1)));
+    cloned_row.html(cloned_row.html().replace('advanced-search-by-' + index, 'advanced-search-by-' + (index + 1)));
+    cloned_row.html(cloned_row.html().replace('element-relation-' + index, 'element-relation-' + (index + 1)));
+    cloned_row.html(cloned_row.html().replace('add_search_elements(' + index + ');', 'add_search_elements(' + (index + 1) + ');'));
+    cloned_row.attr('id', 'advanced-search-row-' + (index + 1));
+
+    cloned_row.insertAfter($("#advanced-search-row-" + index));
+    //console.log('jees');
   }
 
 }
