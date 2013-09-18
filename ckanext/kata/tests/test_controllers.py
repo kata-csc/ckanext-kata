3bdd123afd2eed5a2acc4393d13d55dddfc81784
Pinja Koskinen <pinja.koskinen@csc.fi>
2013-09-18 11:17:27 +0300

    Added test for reporting and system pages

--- ckanext/kata/tests/test_controllers.py
+++ ckanext/kata/tests/test_controllers.py
@@ -120,3 +120,18 @@ class TestKataControllers(WsgiAppCase, HtmlCheckMethods, CommonFixtureMethods):
         assert '<label class="control-label" for="new_comment">' in res, 'Text area to add a comment should exist'
         assert '<label for="rating-3"' in res, 'Rating should exist in add comment page'
         assert res.status == 200, 'Wrong status code in new comment page'
+        
+    def test_admin_system_reporting_rendered(self):
+        '''
+        An admin user should see system info and reportin pages.
+        '''
+        offset = url_for(controller='ckanext.kata.controllers:SystemController', action='system')
+        username = u'testsysadmin'.encode('utf8')
+        extra_environ = {'REMOTE_USER':username}
+        res = self.app.get(offset, extra_environ=extra_environ)
+        assert res.status==200, 'Wrong status code (should be 200), in admin/system'        
+        offset = url_for(controller='ckanext.kata.controllers:SystemController', action='report')
+        username = u'testsysadmin'.encode('utf8')
+        extra_environ = {'REMOTE_USER':username}
+        res = self.app.get(offset, extra_environ=extra_environ)
+        assert res.status==200, 'Wrong status code (should be 200), in admin/reporting'
