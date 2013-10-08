b6301da36de8b3f962b530c8e6ce8bb5c84845fc
Mikko Koho <mikko.koho@csc.fi>
2013-10-08 15:12:39 +0300

    Disable some non-helpful pylint messages. Revert table instances back to public.

--- ckanext/kata/model.py
+++ ckanext/kata/model.py
@@ -1,4 +1,6 @@
-'''Model additions for Kata'''
+# pylint: disable=invalid-name, no-member
+
+'''Database model additions for Kata'''
 
 import datetime
 import logging
@@ -85,12 +87,12 @@ def setup():
     '''
     Creates the tables that are specified in this file
     '''
-    if model.package_table.exists() and not _kata_access_request_table.exists():
-        _kata_access_request_table.create()
+    if model.package_table.exists() and not kata_access_request_table.exists():
+        kata_access_request_table.create()
         log.debug('Kata access request table created')
 
-    if model.user_table.exists() and not _user_extra_table.exists():
-        _user_extra_table.create()
+    if model.user_table.exists() and not user_extra_table.exists():
+        user_extra_table.create()
         log.debug('User extra table created')
 
 
@@ -101,11 +103,11 @@ def delete_tables():
 
     #if user_extra_table.exists():
     #user_extra_table.delete()
-    if _kata_access_request_table.exists():
-        _kata_access_request_table.delete()
+    if kata_access_request_table.exists():
+        kata_access_request_table.delete()
 
 
-_kata_access_request_table = Table('kata_req', meta.metadata,
+kata_access_request_table = Table('kata_req', meta.metadata,
                                   Column('id', types.UnicodeText, primary_key=True, default=_types.make_uuid),
                                   Column('pkg_id', types.UnicodeText, nullable=False),
                                   Column('user_id', types.UnicodeText, nullable=False),
@@ -113,20 +115,20 @@ _kata_access_request_table = Table('kata_req', meta.metadata,
                                   UniqueConstraint('pkg_id', 'user_id', name='pkgusr_1'),
                                   )
 
-mapper(KataAccessRequest, _kata_access_request_table, extension=[
+mapper(KataAccessRequest, kata_access_request_table, extension=[
     extension.PluginMapperExtension(),
     ])
 
 
-_user_extra_table = Table('user_extra', meta.metadata,
+user_extra_table = Table('user_extra', meta.metadata,
                          Column('id', types.UnicodeText, primary_key=True, default=_types.make_uuid),
                          Column('user_id', types.UnicodeText, ForeignKey('user.id')),
                          Column('key', types.UnicodeText),
                          Column('value', _types.JsonType),
                          )
-vdm.sqlalchemy.make_table_stateful(_user_extra_table)
+vdm.sqlalchemy.make_table_stateful(user_extra_table)
 
-meta.mapper(UserExtra, _user_extra_table, properties={
+meta.mapper(UserExtra, user_extra_table, properties={
     'user': orm.relation(user.User,
                          backref=orm.backref('_extras',
                                              collection_class=orm.collections.attribute_mapped_collection(u'key'),
@@ -134,10 +136,11 @@ meta.mapper(UserExtra, _user_extra_table, properties={
                                              ),
                          )
 },
-            order_by=[_user_extra_table.c.user_id, _user_extra_table.c.key],
+            order_by=[user_extra_table.c.user_id, user_extra_table.c.key],
             )
 
-_extras_active = vdm.sqlalchemy.stateful.DeferredProperty('_extras',
-                                                          vdm.sqlalchemy.stateful.StatefulDict, base_modifier=lambda x: x.get_as_of())
-setattr(user.User, 'extras_active', _extras_active)
+extras_active = vdm.sqlalchemy.stateful.DeferredProperty('_extras',
+                                                          vdm.sqlalchemy.stateful.StatefulDict,
+                                                          base_modifier=lambda x: x.get_as_of())
+setattr(user.User, 'extras_active', extras_active)
 user.User.extras = vdm.sqlalchemy.stateful.OurAssociationProxy('extras_active', 'value', creator=_create_extra)
