786ab9718106e2f1f06ad7192812f67c04f00b8e
Mikko Koho <mikko.koho@csc.fi>
2014-05-07 15:09:53 +0300

    Update version number to 1.0.0

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ version = '1.0.0'
 setup(
     name='ckanext-kata',
     version=version,
-    description="KATA extensions for CKAN",
+    description="KATA extension for CKAN",
     long_description="""Provides modified Add Dataset (package) page and other modifications. This extension contains also jQuery files, custom css (eg. kata.css) and several templates are overwritten from basic CKAN to provide the Kata/TTA looks.""",
     classifiers=[], # Get strings from http://pypi.python.org/pypi?%3Aaction=list_classifiers
     keywords='',
