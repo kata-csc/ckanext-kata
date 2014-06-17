b7eb35904fcfa6382025f7f5a5f4436819d4cc27
Pinja Koskinen <pinja.koskinen@csc.fi>
2014-06-17 08:07:14 +0300

    Version PID is now generated, if the field is left empty and the checkbox is checked.

--- ckanext/kata/actions.py
+++ ckanext/kata/actions.py
@@ -116,8 +116,8 @@ def package_create(context, data_dict):
     # Get version PID (or generate a new one?)
     new_version_pid = data_dict.get('new_version_pid')
 
-    # if not new_version_pid:
-    #     new_version_pid = utils.generate_pid()
+    if not new_version_pid and data_dict.get('generate_version_pid', None) == 'on':
+        new_version_pid = utils.generate_pid()
 
     if new_version_pid:
         data_dict['pids'] = data_dict.get('pids', []) + [{'id': new_version_pid, 'type': 'version', 'provider': 'kata'}]
@@ -193,6 +193,9 @@ def package_update(context, data_dict):
     data_dict['pids'] = temp_pkg_dict.get('pids', [])
 
     new_version_pid = data_dict.get('new_version_pid', None)
+    if not new_version_pid and data_dict.get('generate_version_pid', None) == 'on':
+        new_version_pid = utils.generate_pid()
+        
     if new_version_pid:
         data_dict['pids'] += [{'id': new_version_pid,
                               'type': 'version',
