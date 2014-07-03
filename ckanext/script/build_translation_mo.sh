c20f9b81d94c73e935d0974affba4696d4206863
Pinja Koskinen <pinja.koskinen@csc.fi>
2014-07-03 07:35:48 +0300

    Added script for compiling translation

---
+++ ckanext/script/build_translation_mo.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+HERE=`dirname $0`
+msgcat --use-first \
+    "$HERE/../kata/i18n/fi/LC_MESSAGES/ckan.po" \
+    "$HERE/../../../ckanext-shibboleth/ckanext/repoze/who/shibboleth/i18n/fi/LC_MESSAGES/ckan.po" \
+    "$HERE/../../../ckanext-rems/ckanext/rems/i18n/fi/LC_MESSAGES/ckan.po" \
+    "$HERE/../../../ckan/ckan/i18n/fi/LC_MESSAGES/ckan.po" \
+    | msgfmt - -o "$HERE/../kata/i18n/fi/LC_MESSAGES/ckan.mo"
