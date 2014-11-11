3f18456edf3209923178c8ac3c702d94c798e97c
Mika Wahlroos <mika.wahlroos@csc.fi>
2014-11-11 11:14:07 +0200

    Remove obsolete code

--- ckanext/kata/exceptions.py
+++
@@ -1,9 +0,0 @@
-"""
-Exceptions for use within the Kata CKAN extension.
-
-These custom exceptions should probably be handled within the extension
-rather than throwing them outside of its scope.
-"""
-
-class MailingException(Exception):
-    pass
