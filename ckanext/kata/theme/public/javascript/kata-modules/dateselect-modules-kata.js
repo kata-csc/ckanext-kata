ca5d3118cb903b0d8b51be0a13e71930d1fe2329
Ilja <ilja.pyykkonen@gofore.com>
2014-11-18 13:13:32 +0200

    Catch datepicker exception

--- ckanext/kata/theme/public/javascript/kata-modules/dateselect-modules-kata.js
+++ ckanext/kata/theme/public/javascript/kata-modules/dateselect-modules-kata.js
@@ -54,10 +54,15 @@ this.ckan.module('dateselect-dcmi-kata', function (jQuery, _)
                     tce.datetimepicker('option', 'minDate', tcb.datetimepicker('getDate') );
                 }
             });
-            var parsed = $.datepicker.parseDateTime("yy-mm-dd", "HH:mm:ssz", tcb[0].value, {separator: 'T'}, {separator: 'T'} );
-            if (parsed) {
-                tcb.datetimepicker('setDate', (parsed));
+            try {
+                var parsed = $.datepicker.parseDateTime("yy-mm-dd", "HH:mm:ssz", tcb[0].value, {separator: 'T'}, {separator: 'T'} );
+                if (parsed) {
+                    tcb.datetimepicker('setDate', (parsed));
+                }
+            } catch(e) {
+                // pass
             }
+
             tce.datetimepicker({
                 timeFormat: "HH:mm:ssz",
                 separator: 'T',
@@ -82,9 +87,14 @@ this.ckan.module('dateselect-dcmi-kata', function (jQuery, _)
                     tcb.datetimepicker('option', 'maxDate', tce.datetimepicker('getDate') );
                 }
             });
-            var parsed = $.datepicker.parseDateTime("yy-mm-dd", "HH:mm:ssz", tce[0].value, {separator: 'T'}, {separator: 'T'} );
-            if (parsed) {
-                tce.datetimepicker('setDate', (parsed));
+
+            try {
+                var parsed = $.datepicker.parseDateTime("yy-mm-dd", "HH:mm:ssz", tce[0].value, {separator: 'T'}, {separator: 'T'} );
+                if (parsed) {
+                    tce.datetimepicker('setDate', (parsed));
+                }
+            } catch(e) {
+                // pass
             }
         },
     };
