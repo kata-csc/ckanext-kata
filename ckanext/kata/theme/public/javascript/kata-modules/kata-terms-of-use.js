ec4232d972df1d82313c8d277de16b4efe0daf30
Ilja <ilja.pyykkonen@gofore.com>
2014-12-05 14:09:40 +0200

    Do not bug about terms if already accepted

--- ckanext/kata/theme/public/javascript/kata-modules/kata-terms-of-use.js
+++ ckanext/kata/theme/public/javascript/kata-modules/kata-terms-of-use.js
@@ -3,9 +3,11 @@ this.ckan.module('kata-terms-of-use', function (jQuery, _) {
     initialize: function () {
       jQuery.proxyAll(this, /_on/);
       this.el.on('change', this._onChange);
+      this._onChange();
     },
+
     _onChange: function () {
-      val = this.el.prop('checked') ? false : true;
+      var val = this.el.prop('checked') ? false : true;
       document.getElementsByName("save")[0].disabled = val;
       document.getElementsByName("save")[1].disabled = val;
     }
