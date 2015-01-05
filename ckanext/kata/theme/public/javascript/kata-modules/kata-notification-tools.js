303cc8ec3495ff07d4cd832d00446039a744e374
Ilja <ilja.pyykkonen@gofore.com>
2015-01-05 15:50:08 +0200

    Formatting fixes

--- ckanext/kata/theme/public/javascript/kata-modules/kata-notification-tools.js
+++ ckanext/kata/theme/public/javascript/kata-modules/kata-notification-tools.js
@@ -7,13 +7,14 @@
 
 this.ckan.module('kata-notification-tools', function (jQuery, _) {
   return {
-	  initialize: function () {
-	      jQuery.proxyAll(this, /_on/);
-	      this.el.on('click', this._onClick);
-	  },
-	  // Closes the calling element from a mouse click
-	  _onClick: function () {
-		  this.el.hide();
-	  }
+    initialize: function () {
+      jQuery.proxyAll(this, /_on/);
+      this.el.on('click', this._onClick);
+    },
+
+    // Closes the calling element from a mouse click
+    _onClick: function () {
+      this.el.hide();
+    }
   };
 });
