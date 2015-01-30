49f2fa5fb27d3c865a0808d92e9054ae1d44c405
Mikko Koho <mikko.koho@csc.fi>
2015-01-30 16:07:13 +0200

    Add logging filter to nose.sh

--- nose.sh
+++ nose.sh
@@ -1,13 +1,14 @@
+#!/bin/sh
 case "$1" in
 	selenium )
 		echo "Running normal tests and selenium tests";
-		nosetests --ckan --with-pylons=ckanext-kata/test-core.ini ckanext-kata/ckanext/kata/tests --logging-clear-handlers
+		nosetests --ckan --with-pylons=ckanext-kata/test-core.ini ckanext-kata/ckanext/kata/tests --logging-clear-handlers --logging-filter=ckanext
 		xvfb-run nosetests ckanext-kata/ckanext/kata/testselenium;
 		;;
 	* )
 		echo "Accepted parameter is 'selenium' to run Selenium tests after normal tests.";
 		echo "Running normal tests";
 
-		nosetests --ckan --with-pylons=ckanext-kata/test-core.ini ckanext-kata/ckanext/kata/tests --logging-clear-handlers
+		nosetests --ckan --with-pylons=ckanext-kata/test-core.ini ckanext-kata/ckanext/kata/tests --logging-clear-handlers --logging-filter=ckanext
 		;;
 esac
