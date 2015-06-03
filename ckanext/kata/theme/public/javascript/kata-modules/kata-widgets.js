0120bab0be7f4f3d483e10d5d0521c1c6d6ee068
Antti Pekka Vilkko <antti.pekka.vilkko@gofore.com>
2015-06-03 12:19:25 +0300

    888, search page renewal

--- ckanext/kata/theme/public/javascript/kata-modules/kata-widgets.js
+++ ckanext/kata/theme/public/javascript/kata-modules/kata-widgets.js
@@ -21,3 +21,43 @@ this.ckan.module('kata-accordion', function ($, _) {
     }
   };
 });
+
+
+this.ckan.module('kata-facet-accordion', function ($, translate) {
+  return {
+    initialize: function () {
+      $.proxyAll(this, /_on/, /_bind/);
+
+      var el = this.el;
+      var collapses = el.find('.collapse');
+      collapses.collapse({toggle: false});
+      el.find('.accordion-toggle').addClass('collapsed');
+
+      var data = this.options.facets;
+      if (!data) {
+        return;
+      }
+
+      // Open the facet accordion if any facet choices are active
+      var limitsChanged = _.any(_.values(data.search));
+      var shouldBeOpen = _.any(data.fields) || limitsChanged;
+      if (shouldBeOpen) {
+        collapses.eq(0).collapse('show');
+      }
+
+      // Setup a toggler for the additional facets not shown initially
+      setTimeout(this._bindCollapses, 200);
+    },
+
+    _onToggle: function () {
+      this.el.find('.etsin-facet-list.collapse').collapse('toggle');
+    },
+
+    _bindCollapses: function () {
+      var collapses = this.el.find('.etsin-facet-list.collapse');
+      collapses.collapse();
+      var toggler = this.el.find('#etsin-facet-list-toggle');
+      toggler.on('click', this._onToggle);
+    }
+  };
+});
