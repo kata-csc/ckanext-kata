4d0853e04eb5c33629c710e5832c1ba65c54e6ef
Peter Kronström <peter.kronstrom@gofore.com>
2016-11-14 16:06:33 +0200

    Update translations for ckanext-showcase

--- ckanext/script/build_translation_mo.sh
+++ ckanext/script/build_translation_mo.sh
@@ -8,6 +8,7 @@ msgcat --use-first \
     "$HERE/../../../ckan/ckan/i18n/fi/LC_MESSAGES/ckan.po" \
     "$HERE/../../../ckanext-statreports/ckanext/statreports/i18n/fi/LC_MESSAGES/ckan.po" \
     "$HERE/../../../ckanext-ytp-comments/i18n/fi/LC_MESSAGES/ckanext-ytp-comments.po" \
+    "$HERE/../../../ckanext-showcase/ckanext/showcase/i18n/fi/LC_MESSAGES/ckanext-showcase.po" \
     | msgfmt - -o "$HERE/../kata/i18n/fi/LC_MESSAGES/ckan.mo"
 
 echo "Done."
