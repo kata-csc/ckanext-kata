0b57d4082cbe24ebd80244f8ae9932bde3529581
junsk1 <junnu.lehtinen@gmail.com>
2017-01-23 13:38:30 +0200

    KATA-1218: Modify Kylli as creator_user_id for Paituli data sets
    Remove waste

--- ckanext/kata/commands/kata.py
+++ ckanext/kata/commands/kata.py
@@ -40,8 +40,6 @@ class Kata(CkanCommand):
             self.sphinx()
         elif cmd == 'crawl':
             self.generate_crawl()
-        elif cmd == 'paituli_change':
-            self.update_paituli()
         else:
             print 'Command %s not recognized' % cmd
 
@@ -77,10 +75,3 @@ class Kata(CkanCommand):
 
         for package in model.Session.query(model.Package).all():
             print self.args[1] % package_dictize(package, {'model': model})
-
-    def update_paituli(self):
-        from sqlalchemy import and_
-        from sqlalchemy import update
-        ses = model.Session
-        ses.execute(update(model.Package).where(and_(model.Package.creator_user_id == '5f1f5463-6943-4610-968b-57a137e4e7f7',model.Package.name.like('urn-nbn-fi-csc-kata000010000%'))).values(creator_user_id = '5adaebc9-920f-4172-8f70-9797cf1c74ce'))
-        ses.commit()
